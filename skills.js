e888b681ae2fee6bd72f0c24c4f903d3990a89da
Ricardo Rafael Quintero Meza, PhD <84739791+ricardo-qm@users.noreply.github.com>
2023-11-02 19:56:59 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,5 @@
+function calculateNumbers(var1, var2)   {
+    var sumTotal = var1 + var2;
+    console.log("The sum of " + var1 + " and " + var2 + " is " + sumTotal + ".");
+    return sumTotal;
+}
